7ef56474e8ce0c9023eff0bfe559c81a6e70531d
David Soliar <dav.soliar@gmail.com>
2024-01-24 15:28:03 +0100

    Add files via upload

---
+++ HomeA1/program_2x_7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float a = 1, b = 2, c = 3, d = 4, e = 50, f = 1000, g = 1002.14;
+
+    double sum = (a+b+c+d+e+f+g)/7;
+
+    printf("%f", sum);
+
+    return 0;
+}
