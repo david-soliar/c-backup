d492a48f2f5b7819fdb1c1f2ad9131540cc2b435
David Soliar <dav.soliar@gmail.com>
2024-01-25 20:33:15 +0100

    Add files via upload

---
+++ HomeA1/program_2x_9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char lower = 'a';
+    char upper = lower-32;
+    /*printf("%i\n", lower);*/
+    printf("%c\n", lower);
+    /*printf("%i\n", upper);*/
+    printf("%c\n", upper);
+    return 0;
+}
